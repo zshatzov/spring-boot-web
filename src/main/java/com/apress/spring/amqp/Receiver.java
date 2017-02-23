cfdf6be807c557367fdf10be70e6ad1bf615c74a
zenovy shatzov <zshatzov@gmail.com>
2017-02-23 11:08:20 -0500

    Removed RxJava rabbitMQ message handling.

--- src/main/java/com/apress/spring/amqp/Receiver.java
+++ src/main/java/com/apress/spring/amqp/Receiver.java
@@ -1,42 +1,17 @@
 package com.apress.spring.amqp;
 
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.apress.spring.model.Journal;
 
-import io.reactivex.Single;
-
 @Component
 public class Receiver{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);	
-	private BlockingQueue<Journal> queue = new LinkedBlockingQueue<>();	
-	private ExecutorService executor = newSingleThreadExecutor();
- 
-	public Single<Journal> subscribe() { 
-		return Single.create(emitter-> {
-			executor.submit(()->{
-				LOG.info("Check to emmit events?");
-				Journal event;
-				try {
-					event = queue.take();
-					emitter.onSuccess(event);
-				} catch (InterruptedException e) {				 
-				}				
-			});
-		}); 
-	}	
 	
 	public void receiveMessage(Journal journal){
 		LOG.info("Received new journal with id <" + journal.getId() + ">");
-		queue.offer(journal);
 	}	 
 }
--- src/main/java/com/apress/spring/service/JournalService.java
+++ src/main/java/com/apress/spring/service/JournalService.java
@@ -3,26 +3,18 @@ package com.apress.spring.service;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.apress.spring.amqp.Receiver;
 import com.apress.spring.model.Journal;
 import com.apress.spring.repository.JournalRepository;
-
-import io.reactivex.observers.DisposableSingleObserver;
 
 @Service
 public class JournalService {
 	
 	private JournalRepository journalRepository;
-	
-	@Autowired
-	private Receiver receiver;
 	
 	private static final Logger LOG = 	
 			LoggerFactory.getLogger(JournalService.class);
@@ -30,22 +22,7 @@ public class JournalService {
 	public JournalService() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	@PostConstruct
-	public void setup(){
-		receiver.subscribe().subscribeWith(new DisposableSingleObserver<Journal>() {
-			@Override
-			public void onSuccess(Journal t) {
-			     LOG.info("Procssing new journal entry...");				
-			}
-			
-			@Override
-			public void onError(Throwable e) {
-				LOG.error("Failed to process new journal entry", e);
-			}			
-		});
-	}
-	
+		
 	public void insertData(){
 		LOG.info("Inserting journal data..."); 
 		Journal journal1 = new Journal("Get to know Spring Boot","Today I will learn SpringBoot");
